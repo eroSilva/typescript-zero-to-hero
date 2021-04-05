ee5277fbef7176765d7e27bd03f53b7cf71cd02e
Erô Silva <eronildo.silva@outlook.com>
2021-04-05 18:37:52 -0300

    Add new examples

--- modulo-02/boolean/app.ts
+++ modulo-02/boolean/app.ts
@@ -3,6 +3,18 @@ Sintaxe em declaração de Variável
 let varName: boolean = true;
 Ex.: 
 ------------------------- */
+
+// First example
 let isValid: boolean = true;
 
 console.log(isValid);
+
+
+// Second example
+let done: boolean = false;
+
+if (done) {
+  console.log(`It's done sucessfuly`)
+} else {
+  console.log(`It's not done sucessfuly`)
+}
